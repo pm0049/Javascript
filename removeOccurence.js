62ddd5d0504f8d0c718b0e7e8e0a57b4f45d2bda
Pratik Mulik <120821308+pm0049@users.noreply.github.com>
2023-11-04 09:53:21 +0530

    Update removeOccurence.js

--- removeOccurence.js
+++ removeOccurence.js
@@ -17,4 +17,14 @@ We're using the splice function with two arguments.
 The first argument is the starting index where we'd like to start the removal of elements.
 
 The second argument is the number of elements we'd like to remove beginning at the starting index. In this case we're removing a single element starting at the index of the element we'd like to remove.
+Let's remove any element greater than five from our array:
+
+function greaterThanFive(array) {
+    for(let i = array.length - 1; i >= 0; i--) {
+        if(array[i] <= 5) {
+            array.splice(i, 1);
+        }
+    }
+}
+
 */
