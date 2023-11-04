a56b49ae14985eb6f114f511c3fb0042f5653f48
Pratik Mulik <120821308+pm0049@users.noreply.github.com>
2023-11-04 10:00:10 +0530

    Create objectBasic.js

---
+++ Object/objectBasic.js
@@ -0,0 +1,21 @@
+/*In JavaScript, objects start with a open curly-brace { and end with a close curly-brace }. We can put key-value pairs in between these braces.
+
+Let's take a look at a simple object:
+*/
+const team = {
+    name: "Mets",
+    wins: 86,
+    inPlayoffs: false,
+};
+const order = {
+    pizzas: 3,
+    extraCheese: true,
+    deliveryInstructions: "Round the back, please!",
+};
+
+module.exports = order;
+/*
+In this object team, we have three keys: name, wins and inPlayoffs 
+
+The value associated with name is "Mets", with wins is 86 and with inPlayoffs is false 
+*/
